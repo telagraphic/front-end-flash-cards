b9151c8e26c517549e867ef72fe85d6ba2cbe267
telagraphic <telagraphic@gmail.com>
2019-08-28 19:06:59 -0400

    /card component added

---
+++ _site/fonts/subset.sh
@@ -0,0 +1,5 @@
+pyftsubset "SansForgetica-Regular.otf" --output-file="SansForgetica-Regular.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "SansForgetica-Regular.otf" --output-file="SansForgetica-Regular.woff" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+
+pyftsubset "Optician-Sans.ttf" --output-file="Optician-Sans.woff2" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Optician-Sans.ttf" --output-file="Optician-Sans.woff" --flavor=woff2 --layout-features="ccmp,locl,mark,mkmk,kern" --desubroutinize --unicodes=U+0-10FFFF
